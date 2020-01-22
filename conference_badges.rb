a927b499d2a9fb4720320f2590c043e56a0b5b7d
Danny Soto <87dsoto@gmail.com>
2020-01-22 16:46:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+   name_array = []
    array.each do |name| 
-     return "Hello, my name is #{name}."
+     name_array.push("Hello, my name is #{name}.")
    end
+   return name_array
 end
